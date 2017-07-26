38f11357490c670cdfdd84e1659656c2fb4d38db
binhbt <binhbt@vega.com.vn>
2017-07-26 10:45:50 +0700

    fix alrigothm

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -14,25 +14,28 @@ import java.io.File;
 public class JavaApplication1 {
     private static final String NEW_NAME_ARR ="3|2|1";
     private static final String NEW_PREFIX ="aa";
+    private static final String FOLDER ="C://test";
+    private static final String NEW_FOLDER ="C://test1/";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        changeAllFileName("C://test", ".txt");
+        changeAllFileName(FOLDER, ".txt");
     }
     //D://xyz
     private static void changeAllFileName(String folder, String type){
-        String[] names = NEW_NAME_ARR.split("|");
+        String[] names = NEW_NAME_ARR.split("\\|");
         if(names != null && names.length >0){
             for(int i=0; i<names.length; i++){
-                changeFileNumber(folder,type, NEW_PREFIX, names[i], i+"");
+                System.out.println("pos"+(i+1));
+                changeFileNumber(folder,type, NEW_PREFIX, names[i], (i+1)+"");
             }
         }
     }
-    private static void changeFileNumber(String folder, String type,  String newPrefix, String oldIdx, String newIdx){
+    private static void changeFileNumber(String folder, String type,  String newPrefix, String newIdx, String oldIdx){
         File dir = new File(folder);
-
+        System.out.println("newIdx "+newIdx);
         if (dir.isDirectory()) { // make sure it's a directory
             for (final File f : dir.listFiles()) {
                 try {
@@ -41,7 +44,8 @@ public class JavaApplication1 {
                     if (oldNames != null && oldNames.length >0) {
                         if (oldNames[1] != null) {
                             if(oldNames[1].equals(oldIdx+type)){
-                                File newfile =new File(newPrefix+newIdx+type);
+                                System.out.println("File "+oldIdx+type+">>"+newIdx+type);
+                                File newfile = new File(NEW_FOLDER+newPrefix+"_"+newIdx+type);
                                 if(f.renameTo(newfile)){
                                     System.out.println("Rename succesful");
                                 }else{
